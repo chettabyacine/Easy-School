4f461f031086249e46029c544350748e69a1f8e7
Yacine Chettab <jy_chettab@esi.dz>
2022-04-03 23:12:23 +0200

    Ajoutez des fichiers projet.

---
+++ UI_01_connexion.Designer.cs
@@ -0,0 +1,39 @@
+﻿namespace Easy_School
+{
+    partial class UI_01_connexion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "UI_01_connexion";
+        }
+
+        #endregion
+    }
+}
